3411014f9827426dd06990fca0ded04560bcbb08
Zhizhen He <hezhizhen.yi@gmail.com>
2025-08-18 13:19:50 +0800

    feat: add more info to version

--- Makefile
+++ Makefile
@@ -2,17 +2,24 @@ PROJECT_PKG = github.com/hezhizhen/sak
 CLI_EXE     = sak
 CLI_PKG     = $(PROJECT_PKG)/cmd/sak
 GIT_COMMIT  = $(shell git rev-parse HEAD)
+GIT_BRANCH  = $(shell git rev-parse --abbrev-ref HEAD)
 GIT_TAG     = $(shell git describe --tags --abbrev=0 --exact-match 2>/dev/null)
 GIT_DIRTY   = $(shell test -n "`git status --porcelain`" && echo "dirty" || echo "clean")
+BUILD_DATE  = $(shell date -u '+%Y-%m-%dT%H:%M:%SZ')
+GO_VERSION  = $(shell go version | cut -d' ' -f3)
 LDFLAGS = -w
 ifdef VERSION
-	LDFLAGS += -X $(PROJECT_PKG)/pkg/version.BuildMetadata=$(VERSION)
+LDFLAGS += -X '$(PROJECT_PKG)/pkg/version.BuildMetadata=$(VERSION)'
 endif
 ifneq ($(GIT_TAG),)
-	LDFLAGS += -X $(PROJECT_PKG)/pkg/version.BuildMetadata=
+LDFLAGS += -X '$(PROJECT_PKG)/pkg/version.BuildMetadata='
 endif
-LDFLAGS += -X $(PROJECT_PKG)/pkg/version.GitCommit=${GIT_COMMIT}
-LDFLAGS += -X $(PROJECT_PKG)/pkg/version.GitTreeState=${GIT_DIRTY}
+LDFLAGS += -X '$(PROJECT_PKG)/pkg/version.GitCommit=$(GIT_COMMIT)'
+LDFLAGS += -X '$(PROJECT_PKG)/pkg/version.GitBranch=$(GIT_BRANCH)'
+LDFLAGS += -X '$(PROJECT_PKG)/pkg/version.GitTag=$(GIT_TAG)'
+LDFLAGS += -X '$(PROJECT_PKG)/pkg/version.GitTreeState=$(GIT_DIRTY)'
+LDFLAGS += -X '$(PROJECT_PKG)/pkg/version.BuildDate=$(BUILD_DATE)'
+LDFLAGS += -X '$(PROJECT_PKG)/pkg/version.GoVersion=$(GO_VERSION)'
 
 .PHONY: test
 test:
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,6 +1,10 @@
 // Package version provides versioning information for the sak application.
 package version
 
+import (
+	"runtime"
+)
+
 var (
 	// Version is the current version of the sak.
 	Version = "0.0.1"
@@ -10,6 +14,14 @@ var (
 	GitCommit = ""
 	// GitTreeState is the state of the git tree
 	GitTreeState = ""
+	// GitBranch is the git branch
+	GitBranch = ""
+	// GitTag is the git tag
+	GitTag = ""
+	// BuildDate is when the binary was built
+	BuildDate = ""
+	// GoVersion is the Go version used to build
+	GoVersion = ""
 )
 
 // GetVersion returns the semver string of the version
@@ -18,4 +30,38 @@ func GetVersion() string {
 		return Version
 	}
 	return Version + "+" + BuildMetadata
+}
+
+// BuildInfo contains all build-time information
+type BuildInfo struct {
+	Version       string
+	GitCommit     string
+	GitBranch     string
+	GitTag        string
+	GitTreeState  string
+	BuildDate     string
+	GoVersion     string
+	GoRuntime     string
+	GOOS          string
+	GOARCH        string
+	NumCPU        int
+	BuildMetadata string
+}
+
+// GetBuildInfo returns comprehensive build and runtime information
+func GetBuildInfo() BuildInfo {
+	return BuildInfo{
+		Version:       GetVersion(),
+		GitCommit:     GitCommit,
+		GitBranch:     GitBranch,
+		GitTag:        GitTag,
+		GitTreeState:  GitTreeState,
+		BuildDate:     BuildDate,
+		GoVersion:     GoVersion,
+		GoRuntime:     runtime.Version(),
+		GOOS:          runtime.GOOS,
+		GOARCH:        runtime.GOARCH,
+		NumCPU:        runtime.NumCPU(),
+		BuildMetadata: BuildMetadata,
+	}
 }
