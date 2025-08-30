cfa82072eb452fbeb6b125af79a0448221e941b9
Zhizhen He <hezhizhen.yi@gmail.com>
2025-08-30 13:10:16 +0800

    chore: remove unused error handling

--- pkg/utils/error.go
+++
@@ -1,9 +0,0 @@
-// Package utils provides utility functions for error handling.
-package utils
-
-// CheckError checks if the provided error is not nil and panics if it is.
-func CheckError(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
